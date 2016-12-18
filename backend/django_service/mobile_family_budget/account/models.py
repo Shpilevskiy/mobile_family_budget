d2d82e83f231725924f48e25b13289a7aca64af6
Nick <arioxon@gmail.com>
2016-12-18 20:00:26 +0300

    account models updated

--- backend/django_service/mobile_family_budget/account/models.py
+++ backend/django_service/mobile_family_budget/account/models.py
@@ -1,24 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+
 from datetime import timedelta
-
-
-class Group(models.Model):
-    group_owner = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
-
-    invite_link = models.OneToOneField(
-        RefLink,
-        on_delete=models.CASCADE)
-
-    users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
-
-    name = models.CharField(max_length=30)
-    login = models.CharField(max_length=30, unique=True)
 
 
 class RefLink(models.Model):
@@ -26,3 +10,19 @@ class RefLink(models.Model):
     creation_date = models.DateField(default=timezone.now())
     expire_date = models.DateField(default=timezone.now() + timedelta(days=10))
     activation_count = models.IntegerField(default=5)
+
+
+class Group(models.Model):
+    group_owner = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+')
+
+    invite_link = models.OneToOneField(
+        RefLink,
+        on_delete=models.CASCADE)
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    name = models.CharField(max_length=30)
+    login = models.CharField(max_length=30, unique=True)
--- backend/django_service/mobile_family_budget/mobile_family_budget/settings.py
+++ backend/django_service/mobile_family_budget/mobile_family_budget/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'account.apps.AccountConfig'
+    'account.apps.AccountConfig',
+    'purchaseManager.apps.PurchasemanagerConfig'
 ]
 
 MIDDLEWARE = [
