cd3c845d9d74eda70630482fe2f267aed161d504
Nick <arioxon@gmail.com>
2017-01-08 22:20:08 +0300

    Base TestCase added

---
+++ backend/django_service/mobile_family_budget/account/tests/base.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework.test import APITestCase
+
+User = get_user_model()
+
+
+class BaseCase(APITestCase):
+    def create_user(self, username, password):
+        user = User(username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def login(self, username='first_user', password='password_1'):
+        a = self.client.login(username=username,
+                              password=password)
+        print(a)
+
+    def logout(self):
+        self.client.logout()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.FIRST_USER_USERNAME = 'first_user'
+        cls.SECOND_USER_USERNAME = 'second_user'
+        cls.FIRST_USER_PASSWORD = 'password_1'
+        cls.SECOND_USER_PASSWORD = 'password_2'
+        super().setUpClass()
+
+    def setUp(self):
+        self.user = self.create_user(username=self.FIRST_USER_USERNAME,
+                                     password=self.FIRST_USER_PASSWORD)
+        super().setUp()
+
+    def tearDown(self):
+        self.logout()
+        self.user.delete()
+        super().tearDown()
