0e46d3846b24c931faaa99b17e5bf6d92d8c6561
Nick <arioxon@gmail.com>
2017-01-21 22:02:01 +0300

    PurchaseManager models were registered.

--- backend/django_service/mobile_family_budget/purchaseManager/admin.py
+++ backend/django_service/mobile_family_budget/purchaseManager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from purchaseManager.models import (
+    Purchase,
+    PurchaseList
+)
+
+admin.site.register(Purchase)
+admin.site.register(PurchaseList)
+
 # Register your models here.
